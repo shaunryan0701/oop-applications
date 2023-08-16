8f54559c8cffaa3cb1c23fa0d9eee9cbf5b972af
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-08-16 10:31:30 +0100

    Added Geometry Game

---
+++ App-1-Geometry-Game/main.py
@@ -0,0 +1,25 @@
+import turtle
+
+myturtle = turtle.Turtle()
+
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+
+turtle.done()
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
