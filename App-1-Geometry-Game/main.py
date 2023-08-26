5fd40e49e0754d8f1eb0f57f8c3198589b1cc4ac
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-08-26 20:40:17 +0100

    Uodated App-1

--- App-1-Geometry-Game/main.py
+++ App-1-Geometry-Game/main.py
@@ -1,25 +1,77 @@
 import turtle
+from random import randint
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.point1.x < self.x < rectangle.point2.x \
+                and rectangle.point1.y < self.y < rectangle.point2.y:
+            return True
+        else:
+            return False
+
+
+class Rectangle:
+
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def area(self):
+        return (self.point2.x - self.point1.x) * \
+               (self.point2.y - self.point1.y)
+
+
+class GuiRectangle(Rectangle):
+
+    def draw(self, canvas):
+        canvas.penup()
+        canvas.goto(self.point1.x, self.point1.y)
+
+        canvas.pendown()
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+        canvas.left(90)
+        canvas.forward(self.point2.x - self.point1.x)
+        canvas.left(90)
+        canvas.forward(self.point2.y - self.point1.y)
+
+
+class GuiPoint(Point):
+
+    def draw(self, canvas, size=5, colour='red'):
+        canvas.penup()
+        canvas.goto(self.x,  self.y)
+        canvas.pendown()
+        canvas.dot(size, colour)
+
+
+# Create rectangle object
+rectangle = GuiRectangle(Point(randint(0, 400), randint(0, 400)),
+                Point(randint(10, 400), randint(10, 400)))
+
+# Print rectangle coordinates
+print("Rectangle Coordinates: ",
+      rectangle.point1.x, ",",
+      rectangle.point1.y, "and",
+      rectangle.point2.x, ",",
+      rectangle.point2.y)
+
+# Get point and area from user
+user_point = GuiPoint(float(input("Guess x: ")), float(input("Guess y: ")))
+user_area = float(input("Guess rectangle area: "))
+
+# Print out the game result
+print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
+print("Your area was off by: ", rectangle.area() - user_area)
 
 myturtle = turtle.Turtle()
-
-myturtle.forward(100)
-myturtle.left(90)
-myturtle.forward(200)
-myturtle.left(90)
-myturtle.forward(100)
-myturtle.left(90)
-myturtle.forward(200)
+rectangle.draw(canvas=myturtle)
+user_point.draw(canvas=myturtle)
 
 turtle.done()
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
