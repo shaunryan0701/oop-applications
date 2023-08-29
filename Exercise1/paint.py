4a8489b1487a2c7e32ecf3875933bb74e56c73c2
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-08-29 11:35:56 +0100

    Uodated Exercise1

--- Exercise1/paint.py
+++ Exercise1/paint.py
@@ -9,3 +9,8 @@ class Paint:
           return self.buckets * 1.99
         else:
           return self.buckets * 2.19
+        
+class DiscountedPaint(Paint):
+
+    def discounted_price(self, discount_percentage):
+       return self.total_price() - (discount_percentage/100 * self.total_price())
