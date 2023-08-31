5df063accb73f69784b1a7ffc0b2edee22792684
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-08-31 14:58:40 +0100

    Uodated App-2

--- App-2-Flatmates-Bill/main.py
+++ App-2-Flatmates-Bill/main.py
@@ -2,6 +2,7 @@
 Object that contains the data about a bill,such as
 total amount and period of the bill
 """
+from fpdf import FPDF
 
 
 class Bill:
@@ -27,6 +28,7 @@ class FlatMate:
         weight = self.days_in_house / (self.days_in_house + flatmate2.days_in_house)
         return bill.amount * weight
 
+
 """
 Creates a pdf file that contains data about the flatmates
 such as their names, their due amount and the period of 
@@ -40,7 +42,30 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+
+        pdf = FPDF(orientation='P', unit='pt')
+        pdf.add_page()
+
+        # Add title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatmate Bill', border=0, align='C', ln=1)
+
+        # Insert period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt='Period', border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=40, txt=self.format_pay_amount(bill, flatmate1, flatmate2), border=0, ln=1)
+
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=40, txt=self.format_pay_amount(bill, flatmate2, flatmate1), border=0, ln=1)
+
+        pdf.output(self.filename)
+
+    @staticmethod
+    def format_pay_amount(bill, flatmate1, flatmate2):
+        return str(round(flatmate1.pays(bill=bill, flatmate2=flatmate2), 2))
 
 
 bill_amount = 120
@@ -49,7 +74,9 @@ the_bill = Bill(amount=bill_amount, period="March 2023")
 dave = FlatMate(name='Dave', days_in_house=27)
 geoff = FlatMate(name='Geoff', days_in_house=20)
 
-
 print('Dave pays', dave.pays(bill=the_bill, flatmate2=geoff))
 print('Geoff pays', geoff.pays(bill=the_bill, flatmate2=dave))
+
+pdf_report = PdfReport(filename='Report1.pdf')
+pdf_report.generate(dave, geoff, the_bill)
 
