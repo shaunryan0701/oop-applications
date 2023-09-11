0d2340721f151949a3621ba97abe9c4a7230e277
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-09-11 14:56:02 +0100

    Refactored App-2

---
+++ App-2-Flatmates-Bill/bill.py
@@ -0,0 +1,11 @@
+"""
+Object that contains the data about a bill,such as
+total amount and period of the bill
+"""
+
+
+class Bill:
+
+    def __init__(self, amount, period):
+        self.amount = amount
+        self.period = period
