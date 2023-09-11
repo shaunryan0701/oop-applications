5d1f9eb17e986d35abfadf4df61bbaef053c14ed
shaunryan0701 <82238574+shaunryan0701@users.noreply.github.com>
2023-09-11 14:57:40 +0100

    Refactored App-2

---
+++ App-2-Flatmates-Bill/pdfreport.py
@@ -0,0 +1,43 @@
+"""
+Creates a pdf file that contains data about the flatmates
+such as their names, their due amount and the period of
+the bill
+"""
+
+
+import webbrowser
+from fpdf import FPDF
+
+
+class PdfReport:
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def generate(self, flatmate1, flatmate2, bill):
+
+        pdf = FPDF(orientation='P', unit='pt')
+        pdf.add_page()
+
+        # Add title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatmate Bill', border=0, align='C', ln=1)
+
+        # Insert period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt='Period:', border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        pdf.cell(w=100, h=40, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=40, txt=self.format_pay_amount(bill, flatmate1, flatmate2), border=0, ln=1)
+
+        pdf.cell(w=100, h=40, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=40, txt=self.format_pay_amount(bill, flatmate2, flatmate1), border=0, ln=1)
+
+        pdf.output(self.filename)
+
+        webbrowser.open('file://' + os.path.realpath(self.filename))
+
+    @staticmethod
+    def format_pay_amount(bill, flatmate1, flatmate2):
+        return str(round(flatmate1.pays(bill=bill, other_flatmate=flatmate2), 2))
